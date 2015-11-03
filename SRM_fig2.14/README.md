acd37c55d72549b5a525bd33d129e9c7351f9470
Shi Chen <shichen@Administrators-MacBook-Pro.local>
2015-11-03 11:39:43 +0100

    code add metainfo

---
+++ SRM_fig2.14/README.md
@@ -0,0 +1,77 @@
+
+![http://quantnet.wiwi.hu-berlin.de/style/banner.png](http://quantnet.wiwi.hu-berlin.de/style/banner.png)
+
+## ![qlogo](http://quantnet.wiwi.hu-berlin.de/graphics/quantlogo.png) **SRM_fig2.14**
+
+
+```yaml
+
+
+Name of QuantLet : SRM_fig2.14
+
+Published in : SRM
+
+Description : 'Produces the QQ plots for four simulated samples of 
+N(0,1), N(5,1), N(0,9) and N(5,9). QQ-plots compare empirical 
+quantiles of a distribution with theoretical quantiles of the standard 
+normal distribution.'
+
+Keywords : 'qq-plot, simulation, normal, normal distribution, plot,
+graphical representation'
+
+See also : 
+
+Author : Sandor
+
+Submitted :
+
+Datafile : 
+
+Example :
+- 'Q-Q-Normal-Plots mit Sollgeraden.'
+
+```
+
+![Picture1](SRM_fig2.14.png)
+
+```R
+
+## clear history
+rm(list = ls(all = TRUE))
+graphics.off()
+
+## install and load packages
+libraries = c("zoo")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+install.packages(x)
+})
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+
+## Q-Q-Normal-Plots mit Sollgeraden
+## Mit festem seed zur Reproduktion
+set.seed(1234)
+w <- rnorm(100)
+set.seed(1234)
+x <- rnorm(100, mean = 5)
+set.seed(1234)
+y <- rnorm(100, sd = 3)
+set.seed(1234)
+z <- rnorm(100, mean = 5, sd = 3)
+
+par(mfrow = c(2, 2))
+qqnorm(w, main = "Normal Q-Q-Plot der Stichprobe w", xlab = "theoretische Standardnormal-Quantile", 
+ylab = "empirische Quantile")
+qqline(w)
+
+qqnorm(x, main = "Normal Q-Q-Plot der Stichprobe x", xlab = "theoretische Standardnormal-Quantile", 
+ylab = "empirische Quantile")
+qqline(x)
+
+qqnorm(y, main = "Normal Q-Q-Plot der Stichprobe y", xlab = "theoretische Standardnormal-Quantile", 
+ylab = "empirische Quantile")
+qqline(y)
+
+qqnorm(z, main = "Normal Q-Q-Plot der Stichprobe z", xlab = "theoretische Standardnormal-Quantile", 
+ylab = "empirische Quantile")
+qqline(z)
+```
---
+++ SRM_fig2.14/SRM_fig2.14.R
@@ -0,0 +1,56 @@
+# ------------------------------------------------------------------------------
+# Project:     SRM - Stochastische Risikomodellierung und statistische Methoden
+# ------------------------------------------------------------------------------
+# Quantlet:    SRM_fig2.14
+# ------------------------------------------------------------------------------
+# Description: Produces the QQ plots for four simulated samples of 
+#              N(0,1), N(5,1), N(0,9) and N(5,9). QQ-plots compare empirical 
+#              quantiles of a distribution with theoretical quantiles of the 
+#              standard normal distribution.
+# ------------------------------------------------------------------------------
+# Keywords:    plot, time-series, graphical representation, visualization,
+#              bond,
+# ------------------------------------------------------------------------------
+# See also:
+# ------------------------------------------------------------------------------
+# Author:      Wellisch
+# ------------------------------------------------------------------------------
+
+## clear history
+rm(list = ls(all = TRUE))
+graphics.off()
+
+## install and load packages
+libraries = c("zoo")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)
+})
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+
+## Q-Q-Normal-Plots mit Sollgeraden
+## Mit festem seed zur Reproduktion
+set.seed(1234)
+w <- rnorm(100)
+set.seed(1234)
+x <- rnorm(100, mean = 5)
+set.seed(1234)
+y <- rnorm(100, sd = 3)
+set.seed(1234)
+z <- rnorm(100, mean = 5, sd = 3)
+
+par(mfrow = c(2, 2))
+qqnorm(w, main = "Normal Q-Q-Plot der Stichprobe w", xlab = "theoretische Standardnormal-Quantile", 
+       ylab = "empirische Quantile")
+qqline(w)
+
+qqnorm(x, main = "Normal Q-Q-Plot der Stichprobe x", xlab = "theoretische Standardnormal-Quantile", 
+       ylab = "empirische Quantile")
+qqline(x)
+
+qqnorm(y, main = "Normal Q-Q-Plot der Stichprobe y", xlab = "theoretische Standardnormal-Quantile", 
+       ylab = "empirische Quantile")
+qqline(y)
+
+qqnorm(z, main = "Normal Q-Q-Plot der Stichprobe z", xlab = "theoretische Standardnormal-Quantile", 
+       ylab = "empirische Quantile")
+qqline(z)
